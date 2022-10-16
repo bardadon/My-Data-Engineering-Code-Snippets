622dbdeaafeb9463d1ab5ec3860b1866fce62482
Bar Dadon <65648983+bardadon@users.noreply.github.com>
2022-10-16 10:02:29 +0300

    Create nth_highest_number_scrach.py

---
+++ Leetcode Questions with a Twist/Nth Highest Salary/Python/nth_highest_number_scrach.py
@@ -0,0 +1,15 @@
+def nth_highest_number(nums, n):
+    n = n - 1
+    
+    for i in range(1, len(nums)):
+
+        temp = nums[i]
+        j = i - 1
+
+        while j>=0 and temp > nums[j]:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = temp
+
+    return nums[n]
+
