ced4800547148e7fa4889d844e532fdb6d80fcb4
Bar Dadon <65648983+bardadon@users.noreply.github.com>
2023-01-04 14:53:20 +0200

    Add files via upload

---
+++ SQL Cookbook/Chapter1 - Retrieving Records/MySQL/1.7_Concatenating_Column_Values.sql
@@ -0,0 +1,21 @@
+select 
+	concat('Employee ', ENAME, ' works as a ', job) as message
+from emp;
+
+/*
+	message
+	Employee WARD works as a SALESMAN
+	Employee TURNER works as a SALESMAN
+	Employee SMITH works as a CLERK
+	Employee SCOTT works as a ANALYST
+	Employee MILLER works as a CLERK
+	Employee MARTIN works as a SALESMAN
+	Employee KING works as a PRESIDENT
+	Employee JONES works as a MANAGER
+	Employee JAMES works as a CLERK
+	Employee FORD works as a ANALYST
+	Employee CLARK works as a MANAGER
+	Employee BLAKE works as a MANAGER
+	Employee ALLEN works as a SALESMAN
+	Employee ADAMS works as a CLERK
+*/
